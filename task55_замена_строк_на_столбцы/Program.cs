a0ef41d507e32a748f8716afec642f233ca2b2b8
Asia84 <84asia@mail.ru>
2022-07-20 00:02:23 +0700

    task55 -меняем строки на столбцы

---
+++ task55_замена_строк_на_столбцы/Program.cs
@@ -0,0 +1,63 @@
+﻿/*Задача 55: Задайте двумерный массив. Напишите программу, которая заменяет строки на столбцы. 
+В случае, если это невозможно, программа должна вывести сообщение для пользователя.
+*/
+
+int[, ] TwoMatrix(int[, ] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+        //int x = Convert.ToInt32(Console.ReadLine());
+        int x = new Random().Next(1, 500);
+        matrix[i , j] = x;
+        }
+    }
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write(matrix[i, j] + "\t");
+        }
+        Console.WriteLine();
+    }
+
+    return matrix;
+}
+
+
+void AvgMatrix(int[, ] matrix)
+{
+    Console.WriteLine();
+    int x;
+    if (matrix.GetLength(0) == matrix.GetLength(1))
+    {
+        for (int i = 0; i < matrix.GetLength(0); i++)
+        {
+            for (int j = 0; j < matrix.GetLength(1); j++)
+            {
+                if (i < j)
+                {
+                    x = matrix[i, j];
+                    matrix[i, j] = matrix[j, i];
+                    matrix[j, i] = x;
+                }
+            }
+        }
+        for (int i = 0; i < matrix.GetLength(0); i++)
+        {
+            for (int j = 0; j < matrix.GetLength(1); j++)
+            {
+                Console.Write(matrix[i, j] + "\t");
+            }
+            Console.WriteLine();
+        }
+    }
+    else
+    Console.WriteLine("Такое невозможно!");
+
+}
+
+int[, ] matrix = new int[4, 4];
+matrix = TwoMatrix(matrix);
+AvgMatrix(matrix);
