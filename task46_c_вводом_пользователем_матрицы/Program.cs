41953b60c66cf6aa8605b776ea540840df4346db
Asia84 <84asia@mail.ru>
2022-07-19 20:23:01 +0700

    task46-ввод пользователем чисел матрицы

---
+++ task46_c_вводом_пользователем_матрицы/Program.cs
@@ -0,0 +1,29 @@
+﻿int[, ] CreateMatr(int n, int m)
+{
+int[, ] matrix = new int[n ,m];
+for (int i = 0; i < matrix.GetLength(0); i++)
+{
+for (int j = 0; j < matrix.GetLength(1); j++)
+{
+matrix[i, j] = Convert.ToInt32(Console.ReadLine());
+}
+}
+return matrix;
+}
+
+
+void PrintMatr(int[, ] matrix)
+{
+for (int i = 0; i < matrix.GetLength(0); i++)
+{
+for (int j = 0; j < matrix.GetLength(1); j++)
+{
+Console.Write(matrix[i, j] + " ");
+}
+Console.WriteLine();
+}
+}
+
+int n = Convert.ToInt32(Console.ReadLine());
+int m = Convert.ToInt32(Console.ReadLine());
+PrintMatr(CreateMatr(n, m));
