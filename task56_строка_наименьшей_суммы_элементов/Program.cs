df6697e13dda1ddcc7db7326e845abdb4b6ced3b
Asia84 <84asia@mail.ru>
2022-07-20 22:40:06 +0700

    task56- поиск минимального значениясуммы какой-то строки с выводом

---
+++ task56_строка_наименьшей_суммы_элементов/Program.cs
@@ -0,0 +1,62 @@
+﻿/*Задача 56/ Задайте прямоугольный двумерный массив. Напишите программу, которая будет находить строку с наименьшей суммой элементов.
+
+Например, задан массив:
+
+1 4 7 2
+
+5 9 2 3
+
+8 4 2 4
+
+5 2 6 7
+Программа считает сумму элементов в каждой строке и выдаёт номер строки с наименьшей суммой элементов: 1 строка
+*/
+
+int[, ] TwoMatrix(int[, ] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+        //int x = Convert.ToInt32(Console.ReadLine());
+        int x = new Random().Next(1, 100);
+        matrix[i , j] = x;
+        }
+    }
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write(matrix[i, j] + "\t");
+        }
+        Console.WriteLine();
+    }
+
+    return matrix;
+}
+
+void ProMatrix(int[, ] matrix)
+{
+    Console.WriteLine();
+    int minrow = 1000;
+    int gmin = 0;
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        int sumrow = 0;
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            sumrow = sumrow + matrix[i, j];
+        }
+        if (sumrow < minrow)
+        {
+            minrow = sumrow;
+            gmin = i + 1;
+        }
+    }
+    Console.WriteLine( " минимальная сумма " + gmin + " строки " +   minrow );
+
+}
+
+int[, ] matrix = new int[3, 3];
+matrix = TwoMatrix(matrix);
+ProMatrix(matrix);
