a0e25e5fb71e8150ce17959858f53886b28cc7e2
Asia84 <84asia@mail.ru>
2022-07-22 18:37:01 +0700

    task69_возведение числа А в степень числа B

---
+++ task69_возведение_числа_A_в_B_степень/Program.cs
@@ -0,0 +1,21 @@
+﻿/*Задача 69: Напишите программу, которая на вход принимает два числа A и B, и возводит число А в целую степень B с помощью рекурсии.
+A = 3; B = 5 -> 243 (3⁵)
+A = 2; B = 3 -> 8
+*/
+
+
+int f(int a, int b)
+{
+    if(b==0)
+        return 1;
+    return f(a, b - 1) *a;
+}
+
+
+Console.WriteLine("Введите число A: ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите число B: ");
+int b = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine($"Число {a} в степени {b} будет равно " + f(a, b));
