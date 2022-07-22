c595e0f59092d76a2ed4ea06a8fe93e9322d4576
Asia84 <84asia@mail.ru>
2022-07-22 12:58:08 +0700

    task57_подсчет чисел в матрице

--- task57_частотный_словарь_сколько_раз_встречается_элемент/Program.cs
+++ task57_частотный_словарь_сколько_раз_встречается_элемент/Program.cs
@@ -44,27 +44,27 @@ int[, ] TwoMatrix(int[, ] matrix)
 
  void AvgMatrix(int[, ] matrix)
 {
-Console.WriteLine();
-int[] array = new int[matrix.GetLength(0) * matrix.GetLength(1)];
-int k = 0;
-for (int i = 0; i < matrix.GetLength(0); i++)
-{
-for (int j = 0; j < matrix.GetLength(1); j++)
-{
-array[k] = matrix[i, j];
-k++;
-}
-}
-for (int i = 0; i < array.Length; i++)
-{
-int count = 1;
-for (int j = 0; j < array.Length; j++)
-{
-if(array[i] == array[j] && i != j)
-count++;
-}
-Console.WriteLine(array[i] + " встречается " + count + " раз");
-}
+    Console.WriteLine();
+    int[] array = new int[matrix.GetLength(0) * matrix.GetLength(1)];
+    int k = 0;
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            array[k] = matrix[i, j];
+            k++;
+        }
+    }
+    for (int i = 0; i < array.Length; i++)
+    {
+        int count = 1;
+        for (int j = 0; j < array.Length; j++)
+        {
+            if(array[i] == array[j] && i != j)
+            count++;
+        }
+        Console.WriteLine(array[i] + " встречается " + count + " раз");
+    }
 
 }
 int[, ] matrix = new int[3, 3];
