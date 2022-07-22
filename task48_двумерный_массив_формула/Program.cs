a8093019e128528e843293a571f49c3ee2e12586
Asia84 <84asia@mail.ru>
2022-07-22 22:53:59 +0700

    Task48 редактирование

--- task48_двумерный_массив_формула/Program.cs
+++ task48_двумерный_массив_формула/Program.cs
@@ -7,28 +7,28 @@ m = 3, n = 4.
 
 int[, ] CreateMatr(int n, int m)
 {
-int[, ] matrix = new int[n ,m];
-for (int i = 0; i < matrix.GetLength(0); i++)
-{
-for (int j = 0; j < matrix.GetLength(1); j++)
-{
-matrix[i, j] = i+j;
-}
-}
-return matrix;
+    int[, ] matrix = new int[n ,m];
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = i+j;
+        }
+    }
+    return matrix;
 }
 
 
 void PrintMatr(int[, ] matrix)
 {
-for (int i = 0; i < matrix.GetLength(0); i++)
-{
-for (int j = 0; j < matrix.GetLength(1); j++)
-{
-Console.Write(matrix[i, j] + " ");
-}
-Console.WriteLine();
-}
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write(matrix[i, j] + " ");
+        }
+        Console.WriteLine();
+    }
 }
 
 int n = Convert.ToInt32(Console.ReadLine());
